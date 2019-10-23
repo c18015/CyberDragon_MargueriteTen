b6352b138b1cefd98aee5c8c37f2027d3c395943
HiroyaMasuko <c18015@std.it-college.ac.jp>
2019-10-23 10:33:11 +0900

    新生Unityデータを追加、 ドラゴンのモデル(仮設置済み)・弓矢のモデル(仮設置済み)・矢を打つプログラム実装

---
+++ CyberDragon_Sotuken2019/Assets/Script/Arrow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    Rigidbody Arrow_Rig;
+    
+    public float speed =  0;
+    public GameObject effect = null;
+   
+
+    public bool TransPos = true;
+    public bool Shot = false;
+
+
+    void Start()
+    {
+        TransPos = true;
+        //Shot = true;
+        Arrow_Rig = this.GetComponent<Rigidbody>();
+
+
+        //
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (TransPos)
+        {
+            GameObject target1 = GameObject.Find("ArrowSetPos");
+            Transform taget2 = target1.GetComponent<Transform>();
+
+            //Debug.Log(taget2);
+            transform.position = taget2.position;
+            transform.rotation = taget2.rotation;
+        }
+       
+
+   
+        if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger))
+        {
+            GameObject LhandPas = GameObject.Find("LHandPos");
+            GameObject RhandPas = GameObject.Find("RHandPos");
+            Vector3 LHP = LhandPas.transform.position;
+            Vector3 RHP = RhandPas.transform.position;
+            speed = Vector3.Distance(LHP, RHP);
+            TransPos = false;
+
+            Shot_qqq();
+            
+
+
+
+
+
+            Destroy(this.gameObject, 30f);
+        }
+
+    }
+
+    void Shot_qqq()
+    {
+        float Power = speed * 2000f;
+        Arrow_Rig.AddForce(transform.forward * Power, ForceMode.Force);
+
+        Debug.Log(Power);
+        
+
+    }
+
+
+    void OnCollisionEnter()//オブジェクトに当たるとエフェクト出して弾丸消滅
+    {
+        Instantiate(effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ CyberDragon_Sotuken2019/Assets/Script/Bow.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public GameObject Arrow = null;
+    public Transform L_HandPos = null;
+    public Vector3 MyVelocity;
+
+
+    bool ArrowInst = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame     collider.gameObject.tag == "Hand" && OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger) || 
+    void Update()
+    {
+       
+
+        if (ArrowInst)
+        {
+            ArrowInstantiate();
+            ArrowInst = false;
+        }
+
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+            ArrowInst = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ArrowInst = true;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        /*if(collider.gameObject.tag == "Hand")
+        {
+            Debug.Log("OnCollideeeeeeeeeer!!!!");
+        }*/
+
+        if (collider.gameObject.tag == "Hand" && OVRInput.Get(OVRInput.RawButton.LIndexTrigger))//右、もしくは左人差し指トリガーを押した時
+        {
+            transform.position = L_HandPos.position;
+            transform.rotation = L_HandPos.rotation;//Quaternion.Euler(HandPos.rotation.x, -90 ,HandPos.rotation.z);
+
+
+            
+
+        }
+
+        if (collider.gameObject.tag == "Arrow")
+        {
+
+        }
+    }
+
+    void ArrowInstantiate()
+    {
+        Instantiate(
+            Arrow,
+            this.transform.position,
+            this.transform.rotation);
+
+        Debug.Log("矢を生み出した");
+    }
+        
+}
+
