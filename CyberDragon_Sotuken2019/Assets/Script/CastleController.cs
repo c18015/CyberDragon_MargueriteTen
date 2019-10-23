0388a91f991e7b97feb5987aac90a1a58f4b7455
c18008 <c18008@std.it-college.ac.jp>
2019-10-23 15:06:12 +0900

    タイトルシーン・チュートリアルシーン・リザルトシーンの追加 シーン遷移プログラムの追加

---
+++ CyberDragon_Sotuken2019/Assets/Script/CastleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        // 移動させたいオブジェクトを取得
+        GameObject obj = GameObject.Find("Title_Castle");
+        // オブジェクトを移動
+        obj.transform.Translate(0.0f, 1.2f, -15.0f);
+    }
+
+}
---
+++ CyberDragon_Sotuken2019/Assets/Script/FadeInController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInController : MonoBehaviour
+{
+    float fadeSpeed = 0.015f;     //透明度が変わるスピードを管理
+    float red, green, blue, alpha;   //パネルの色、不透明度を管理
+    public bool isFadeIn = false;   //フェードイン処理の開始、完了を管理するフラグ
+    Image fadeImage;                //透明度を変更するパネルのイメージ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alpha = fadeImage.color.a;
+    }
+    void Update()
+    {
+        StartFadeIn();
+    }
+
+    void StartFadeIn()
+    {
+        alpha -= fadeSpeed;                //a)不透明度を徐々に下げる
+        SetAlpha();                      //b)変更した不透明度パネルに反映する
+        if (alpha <= 0)
+        {                    //c)完全に透明になったら処理を抜ける
+            isFadeIn = false;
+            fadeImage.enabled = false;    //d)パネルの表示をオフにする
+        }
+    }
+
+    void SetAlpha()
+    {
+        fadeImage.color = new Color(red, green, blue, alpha);
+    }
+}
